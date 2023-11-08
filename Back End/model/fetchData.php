31ca4b9511c346a5db4ec61fb2b65ce8f478cf23
Muhammad Fathurraihan Saputra <fthrn.s14@gmail.com>
2023-11-08 10:08:19 +0700

    pembuatan lobby dashboard

---
+++ Back End/model/fetchData.php
@@ -0,0 +1,18 @@
+<?php
+    // require_once __DIR__ . "/connection.php";
+    // function getTableEmployee(): mixed
+    // {
+    //     $sql = <<<SQL
+    //     SELECT * FROM employee;
+    //     SQL;
+
+    //     return getConnection()->query($sql);
+    // }
+    // function getTableOwner(): mixed
+    // {
+    //     $sql = <<<SQL
+    //     SELECT * FROM owner;
+    //     SQL;
+    //     return getConnection()->query($sql);
+    // }
+?>
