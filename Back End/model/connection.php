0b2aa67d533f4ac01dbbe2d5760b92387c6daad5
Muhammad Fathurraihan Saputra <fthrn.s14@gmail.com>
2023-11-15 09:33:46 +0700

    progres

--- Back End/model/connection.php
+++ Back End/model/connection.php
@@ -1,65 +1,11 @@
 <?php
-    function getConnection(): PDO
+    function mySqlConnection(): mysqli
     {
         $host = "localhost";
-        $port = 3306;
-        $dbname = "shoes_reborn";
         $user = "root";
-        $pass = "";
-
-        return new PDO("mysql:host=$host:$port;dbname=$dbname", $user, $pass);
+        $password = "";
+        $dbname = "shoes_reborn";
+        
+        return mysqli_connect($host, $user, $password, $dbname );
     }
-
-    // try {
-    //     //code...
-    //     getConnection();
-    //     echo "berhasil masuk";
-    // } catch (\Throwable $th) {
-    //     echo "gagal: {$th->getMessage()}";
-    // }
-
-    // try {
-    //     $sql = <<<SQL
-    //     INSERT INTO employee VALUES
-    //     ("Jojo", "Pura", "uwak", "547", "desember");
-    //     SQL;
-    //     getConnection()->exec($sql);
-    //     echo "berhasil";
-    // } catch (\Throwable $th) {
-    //     echo "gagal: {$th->getMessage()}";
-    // }
-
-    $connection = getConnection();
-
-    $username = "admin";
-    $pass= "123";
-
-    // // SQL Injection
-    // $sql = "INSERT INTO employee VALUES
-    // ('wori', 'inab', :username, :pass, 'juli')"; //atau :username dan :pass diganti ? 
-    // $result = $connection->prepare($sql); 
-    // $result->bindParam("username", $username); //username diganti index 1
-    // $result->bindParam("pass", $username); //pass diganti index 2
-    // $result->execute();
-
-    // Query untuk return seperti select
-    $sqlshow = "SELECT * FROM employee";
-    $result = $connection->query($sqlshow);
-    // foreach($result as $row) {
-    // var_dump($row);
-    // }
-
-    
-    while($row = $result->fetchAll(PDO::FETCH_ASSOC))
-    {
-        var_dump($row["employee_name"]) . PHP_EOL; 
-    }
-
-    // if($row = $result->fetch())
-    // {
-    //     echo $row["employee_username"];
-    // } else 
-    // {
-    //     echo "gagal";
-    // }
 ?>
