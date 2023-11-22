cf5d02d6025fd0a04c4f2e7351842e5218f88493
Muhammad Fathurraihan Saputra <fthrn.s14@gmail.com>
2023-11-22 08:38:40 +0700

    penggantian database di connection.php

--- Back End/model/connection.php
+++ Back End/model/connection.php
@@ -4,7 +4,7 @@
         $host = "localhost";
         $user = "root";
         $password = "";
-        $dbname = "shoes_reborn";
+        $dbname = "shoes_reborn004";
         
         return mysqli_connect($host, $user, $password, $dbname);
     }
